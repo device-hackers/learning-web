f5cccb79c3cddc9222ce03c85708b456f50b283b
Andriy Ha <andriy.ha@nextcode.tech>
2025-03-07 21:37:04 +0200

    Fix broken static due to worker

--- docs/auth-bridge.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('message', function(event) {
-    if (event.data && event.data.type === 'SET_TOKEN') {
-        localStorage.setItem('token', event.data.token);
-        displayLocalStorage();
-    } else if (event.data && event.data.type === 'GET_TOKEN') {
-        const token = localStorage.getItem('token') || '';
-        event.source.postMessage({ type: 'TOKEN_RESPONSE', token }, '*');
-    }
-});
