9b33b036c4bf9f61957b69d0d0f7f0b5eb14417b
TheBit <thebit1984@gmail.com>
2026-02-28 13:52:52 +0200

    Pass unit 3.1

--- astro-blog-test/astro.config.mjs
+++ astro-blog-test/astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import unlessSupportsDirective from './src/integrations/astro-css-directives/register';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [react(), unlessSupportsDirective()],
