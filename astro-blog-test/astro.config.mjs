5a8bac0b7520d71fa2b5c1e447bd80d0937bb892
TheBit <thebit1984@gmail.com>
2026-03-01 14:40:23 +0200

    Fix SSR

--- astro-blog-test/astro.config.mjs
+++ astro-blog-test/astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import unlessSupportsDirective from './src/integrations/astro-css-directives/register';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server',
   integrations: [react(), unlessSupportsDirective()],
   site: "https://example.com",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
